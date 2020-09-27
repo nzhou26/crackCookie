21d477179713dbfb7699eea1ac55bce4d5050edf
nzhou26 <43943373+nzhou26@users.noreply.github.com>
2020-09-27 10:48:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var d = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res){
+    res.render("home");
+});
+
+/*num = Math.floor(Math.random() * 5);
+if(num == 1 ){
+    thing = "搓澡";
+}else if (num == 2 ){
+    thing = "挖鼻屎";
+}else {
+    thing = "other";
+};
+*/
+var ranNum =Math.sqrt(d.getDate() * d.getMonth());
+var strNum = ranNum.toString();
+var lastDigit = parseInt(strNum[strNum.length -1]);
+//var doSomething =["搓澡", "挖鼻屎", "吃棒棒糖", "玩动森", "画裸男"];
+//var things;
+if(lastDigit == 1 || lastDigit == 2){
+    things = "搓澡";
+}else if(lastDigit == 3 || lastDigit == 4){
+    things = "挖鼻屎";
+}else if(lastDigit == 5 || lastDigit == 6){
+    things = "吃棒棒糖";
+}else if(lastDigit == 7 || lastDigit == 8){
+    things = "玩动森";
+}else if(lastDigit == 9 || lastDigit == 0){
+    things = "画裸男";
+};
+
+app.get('/result', function(req, res){
+    res.render("result",{
+        month:d.getMonth(),
+        date:d.getDate(),
+        thing:things
+
+    })
+});
+
+app.get('*',function(req, res){
+    res.send("wrong url")
+});
+
+app.listen(3000);
